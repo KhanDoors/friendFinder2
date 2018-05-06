a55f7030577a5fd67ce1b3c8ce96da42568e9da9
unknown <syyadkhan@yahoo.com>
2018-05-06 08:55:10 -0400

    may060855

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Gigi',
+      profilePic: 'https://img.etimg.com/thumb/msid-58146937,width-643,imgsize-226895,resizemode-4/model-gigi-hadid-will-be-on-her-maiden-trip-to-india-to-launch-her-collection.jpg',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
